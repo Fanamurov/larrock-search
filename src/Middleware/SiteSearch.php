19ca1bcb2c0c867ea26961052ef30bb8add0f78e
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-13 12:33:07 +1000

    hot fix

--- src/Middleware/SiteSearch.php
+++ src/Middleware/SiteSearch.php
@@ -16,17 +16,11 @@ class SiteSearch
      */
     public function handle($request, Closure $next)
     {
-        //Cache::flush();
         $data = Cache::remember('siteSearch', 1440, function(){
             $data = [];
-            /*foreach (LarrockCatalog::getModel()->whereActive(1)->with(['get_category'])->get(['id', 'title']) as $item){
-                $data[$item->id]['id'] = $item->id;
-                $data[$item->id]['title'] = $item->title;
-                $data[$item->id]['category'] = $item->get_category->first()->title;
-            }*/
             $config = config('larrock-search.components');
             foreach ($config as $item){
-                if($search_data = $item->search(NULL)){
+                if($search_data = $item->search()){
                     $data = array_merge($data, $search_data);
                 }
             }
